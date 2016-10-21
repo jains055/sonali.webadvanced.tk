9fd86cf698c30f74532176e5f5d55fd465d055f7
jains055 <jains055@newschool.edu>
2016-10-21 07:09:05 +0000

    please

--- domain/script.js
+++ domain/script.js
@@ -1,10 +1,8 @@
 
-            window.addEventListener('deviceorientation', function(event) {
-               document.getElementById('cube').style.webkitTransform =
-               document.getElementById('cube').style.transform =
-                       'rotateX(' + event.beta + 'deg) ' +
+              window.addEventListener('deviceorientation', function(event) {
+                 document.getElementById('cube').style.webkitTransform =
+                 document.getElementById('cube').style.transform =
+                    'rotateX(' + event.beta + 'deg) ' +
                        'rotateY(' + event.gamma + 'deg) ' +
                        'rotateZ(' + event.alpha + 'deg)';
-
-            });
-        
+              });
