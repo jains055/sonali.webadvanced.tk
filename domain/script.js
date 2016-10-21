845733394e4fad5d3ffa6927a1c54ede8f591d81
jains055 <jains055@newschool.edu>
2016-10-21 06:28:20 +0000

    sda

--- domain/script.js
+++ domain/script.js
@@ -1,7 +1,7 @@
 
             window.addEventListener('deviceorientation', function(event) {
-               document.getElementById('cube').style.webkitTransform =
-               document.getElementById('cube').style.transform =
+               document.getElementById('shape').style.webkitTransform =
+               document.getElementById('shape').style.transform =
                        'rotateX(' + event.beta + 'deg) ' +
                        'rotateY(' + event.gamma + 'deg) ' +
                        'rotateZ(' + event.alpha + 'deg)';
