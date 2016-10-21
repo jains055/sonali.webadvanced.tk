9fbc493ab118477f9d50ef826ffa644945c1c6e1
jains055 <jains055@newschool.edu>
2016-10-21 05:59:24 +0000

    sad

--- domain/script.js
+++ domain/script.js
@@ -9,10 +9,7 @@
                        'rotateX(' + event.beta + 'deg) ' +
                        'rotateY(' + event.gamma + 'deg) ' +
                        'rotateZ(' + event.alpha + 'deg)';
- 
-               document.getElementById('beta').innerHTML = Math.round(event.beta);
-               document.getElementById('gamma').innerHTML = Math.round(event.gamma);
-               document.getElementById('alpha').innerHTML = Math.round(event.alpha);
+
             });
          }
  
