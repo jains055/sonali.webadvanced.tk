6a8f901a2e3fddb474f86bdc9bbb78f0821a005a
jains055 <jains055@newschool.edu>
2016-10-28 16:44:35 +0000

    sd

--- domain/script.js
+++ domain/script.js
@@ -1,11 +1,10 @@
 
-              window.addEventListener('deviceorientation', function(event) {
+              window.addEventListener('deviceorientation','devicemotion', function(event) {
                   
                   for (var i = 0; i < 10; i++) {
                      $(".cube").append("<div class='one'></div>");
                      $(".cube").append("<div class='two'></div>");
                     }
-                  
                   
                   
                  document.getElementById('cube').style.webkitTransform =
@@ -14,7 +13,39 @@
                        'rotateY(' + event.gamma + 'deg) ' +
                        'rotateZ(' + event.alpha + 'deg)';
                        
-                       //$(".pattern").css("background-color","rgb(" + event.beta + "," + event.gamma + "," + event.alpha + ")");
+                      // $(".one").css("background-color","rgb(" + event.beta + "," + event.gamma + "," + event.alpha + ")");
+                       
+function deviceMotionHandler(eventData) {
+  var info, xyz = "[X, Y, Z]";
+
+  // Grab the acceleration from the results
+  var acceleration = eventData.acceleration;
+  info = xyz.replace("X", acceleration.x);
+  info = info.replace("Y", acceleration.y);
+  info = info.replace("Z", acceleration.z);
+  document.getElementById("moAccel").innerHTML = info;
+
+  // Grab the acceleration including gravity from the results
+  acceleration = eventData.accelerationIncludingGravity;
+  info = xyz.replace("X", acceleration.x);
+  info = info.replace("Y", acceleration.y);
+  info = info.replace("Z", acceleration.z);
+  document.getElementById("moAccelGrav").innerHTML = info;
+
+  // Grab the rotation rate from the results
+  var rotation = eventData.rotationRate;
+  info = xyz.replace("X", rotation.alpha);
+  info = info.replace("Y", rotation.beta);
+  info = info.replace("Z", rotation.gamma);
+  document.getElementById("moRotation").innerHTML = info;
+
+  // // Grab the refresh interval from the results
+  info = eventData.interval;
+  document.getElementById("moInterval").innerHTML = info;       
+}
+                            
+                            
+ 
               });
               
               
