af5931c0a50cea189944fc6ab739bfa7d90c1daa
jains055 <jains055@newschool.edu>
2016-11-04 16:48:22 +0000

    jhj

--- domain/script.js
+++ domain/script.js
@@ -1,7 +1,7 @@
 
               //creating multiple lines  
                 
-                for (var i = 0; i < 20; i++) {
+                for (var i = 0; i < 70; i++) {
                  $(".cube").append("<div class='one'></div>");
                  $(".cube").append("<div class='two'></div>");
                  console.log(4);
