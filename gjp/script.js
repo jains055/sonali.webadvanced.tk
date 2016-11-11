d0d244c51aa99bf7f739e76609dcd6584bb4428d
jains055 <jains055@newschool.edu>
2016-11-11 02:10:06 +0000

    sv

--- gjp/script.js
+++ gjp/script.js
@@ -13,8 +13,8 @@
     
      window.addEventListener('deviceorientation', function(event) {
         
-        document.getElementById('one').style.webkitTransform =
-        document.getElementById('one').style.transform =
+        document.getElementById('lines').style.webkitTransform =
+        document.getElementById('lines').style.transform =
             'rotateX(' + event.beta + 'deg) ' +
             'rotateY(' + event.gamma + 'deg) ' +
             'rotateZ(' + event.alpha + 'deg)';
