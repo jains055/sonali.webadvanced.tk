98ec57d73b2e194be4b03ae9bda677cc1defbc03
jains055 <jains055@newschool.edu>
2016-11-11 06:48:56 +0000

    czz

--- gjp/script.js
+++ gjp/script.js
@@ -49,46 +49,46 @@
                             
                              window.addEventListener('deviceorientation', function(event) {
                                 
-                            //   var alpha = event.alpha;
-                            //   var beta = event.beta;
-                            //   var gamma = event.gamma;
-                            //   var hue = 0, saturation = 0, lightness = 0, red, blue, green;
+                              var alpha = event.alpha;
+                              var beta = event.beta;
+                              var gamma = event.gamma;
+                              var hue = 0, saturation = 0, lightness = 0, red, blue, green;
                         
-                            //   // use -20 to +20 degrees angle for calculating saturation 
-                            //   // value according to beta angle
-                            //   beta += 20;
-                            //   if (beta < 0) saturation = 0;
-                            //   else if (beta > 40) saturation = 100;
-                            //   else {
-                            //      saturation = 100 / 40 * beta;
-                            //   }
+                              // use -20 to +20 degrees angle for calculating saturation 
+                              // value according to beta angle
+                              beta += 20;
+                              if (beta < 0) saturation = 0;
+                              else if (beta > 40) saturation = 100;
+                              else {
+                                 saturation = 100 / 40 * beta;
+                              }
                         
-                            //   // use -20 to +20 degrees angle for calculating lightness 
-                            //   // value according to gamma angle
-                            //   gamma += 20;
-                            //   if (gamma < 0) lightness = 0;
-                            //   else if (gamma > 40) lightness = 100;
-                            //   else {
-                            //      lightness = 100 / 40 * gamma;
-                            //   }
+                              // use -20 to +20 degrees angle for calculating lightness 
+                              // value according to gamma angle
+                              gamma += 20;
+                              if (gamma < 0) lightness = 0;
+                              else if (gamma > 40) lightness = 100;
+                              else {
+                                 lightness = 100 / 40 * gamma;
+                              }
                         
-                            //   // use the whole 360 degree angle for calculating 
-                            //   // red value according to alpha angle
-                            //   alpha = Math.abs(alpha);
-                            //   hue = alpha;
+                              // use the whole 360 degree angle for calculating 
+                              // red value according to alpha angle
+                              alpha = Math.abs(alpha);
+                              hue = alpha;
                         
-                            //   hue = Math.round(hue);
-                            //   saturation = Math.round(saturation);
-                            //   lightness = Math.round(lightness);
+                              hue = Math.round(hue);
+                              saturation = Math.round(saturation);
+                              lightness = Math.round(lightness);
                         
-                            //   var hslString = "hsl(" + hue + ", " + saturation + "%, " + lightness + "%)";
-                            //   var rgb = hslToRgb(hue / 360, saturation / 100, lightness / 100);
-                            //   red = rgb[0];
-                            //   green = rgb[1];
-                            //   blue = rgb[2];
+                              var hslString = "hsl(" + hue + ", " + saturation + "%, " + lightness + "%)";
+                              var rgb = hslToRgb(hue / 360, saturation / 100, lightness / 100);
+                              red = rgb[0];
+                              green = rgb[1];
+                              blue = rgb[2];
                         
-                            //   var rgbStringInverted = "rgb(" + (255 - red) + ", " + (255 - green) + ", " + (255 - blue) + ")";
-                            //   var hexString = "#" + rgbToHex(red, green, blue);
+                              var rgbStringInverted = "rgb(" + (255 - red) + ", " + (255 - green) + ", " + (255 - blue) + ")";
+                              var hexString = "#" + rgbToHex(red, green, blue);
                                
                                 //$(".one").css("background-color", hslString);
                                 
