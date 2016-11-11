8c202b3657a18e3c03d763073fdbb744552c7b98
jains055 <jains055@newschool.edu>
2016-11-11 03:37:43 +0000

    saca

--- gjp/script.js
+++ gjp/script.js
@@ -25,14 +25,14 @@
         console.log(alpha)
         
         //line 2 width based on beta
-        var beta = (Math.abs(event.beta))/2;
-        $(".two").css("height", ( beta + "px"));
-        console.log(beta)
+        // var beta = (Math.abs(event.beta))/2;
+        // $(".two").css("height", ( beta + "px"));
+        // console.log(beta)
         
         //line 3 width based on gamma
-        var gamma = (Math.abs(event.gamma))/2;
-        $(".three").css("height", ( gamma + "px"));
-        console.log(gamma)
+        // var gamma = Math.abs(event.gamma);
+        // $(".three").css("height", ( gamma + "px"));
+        // console.log(gamma)
         
     });
     
