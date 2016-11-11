80ad9a049587c5bf9062a0cdcaefd527fbe57e7e
jains055 <jains055@newschool.edu>
2016-11-11 00:28:45 +0000

    dasf

---
+++ gjp/script.js
@@ -0,0 +1,6 @@
+for (var i = 0; i <70; i++) {
+                 $(".lines").append("<div class='one'></div>");
+                 $(".lines").append("<div class='two'></div>");
+                 $(".lines").append("<div class='three'></div>");
+                 console.log(3);
+                   }
