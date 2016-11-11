612ed902da285cc668c49e5a21c80f8fd01bf1ce
jains055 <jains055@newschool.edu>
2016-11-11 02:09:21 +0000

    va

--- gjp/script.js
+++ gjp/script.js
@@ -1,9 +1,25 @@
-document.ontouchmove = function(e){ e.preventDefault(); }
+    //dissable scroll
+    document.ontouchmove = function(e){ e.preventDefault(); }
 
+    //append multiple lines
+    for (var i = 0; i <34; i++) {
+        $(".lines").append("<div class='one'></div>");
+        $(".lines").append("<div class='two'></div>");
+        $(".lines").append("<div class='three'></div>");
+        console.log(3);
+    }
+    
+    //enable rotation
+    
+     window.addEventListener('deviceorientation', function(event) {
+        
+        document.getElementById('one').style.webkitTransform =
+        document.getElementById('one').style.transform =
+            'rotateX(' + event.beta + 'deg) ' +
+            'rotateY(' + event.gamma + 'deg) ' +
+            'rotateZ(' + event.alpha + 'deg)';
+                       
+    });
+    
 
-for (var i = 0; i <34; i++) {
-                 $(".lines").append("<div class='one'></div>");
-                 $(".lines").append("<div class='two'></div>");
-                 $(".lines").append("<div class='three'></div>");
-                 console.log(3);
-                   }
+                
